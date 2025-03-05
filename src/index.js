afa2bb621e65986a1f0ff7e017bd6b693542daf4
Sakshi Verma <sakshi02v@gmail.com>
2025-03-05 13:30:34 +0530

    [Sakshi Verma]Add. Ability to ensure there is no Duplicate Entry of the same Person in the Address Book

--- src/index.js
+++ src/index.js
@@ -3,8 +3,36 @@ const Contact = require('./models/Contact');
 
 const addressBook = new AddressBook();
 
-const contact1 = new Contact(
-    "Sakshi", "Verma", "Bhopal", "India", "In", "10001", "0899567890", "sakshi02v@gmail.com"
-);
-console.log(addressBook.addContact(contact1));
-console.log(addressBook.getAllContacts());
+try {
+    const contact1 = new Contact(
+        "Sakshi", "Verma", "Bhopal", "India", "In", "100001", "9998887771","sakshi02v@gmail.com"
+    );
+    console.log(addressBook.addContact(contact1)); 
+
+    const contact2 = new Contact(
+        "Ram", "Sharma", "Bhopal", "India","In","1000000","9166547988","sharmaa56@gmail.com"
+    )
+    console.log(addressBook.addContact(contact2)); 
+    console.log("All Contacts Before editing", addressBook.getAllContacts());
+    console.log(addressBook.editContact("Rakesh", { address: "Pipariya", city: "New York", phone: "9234567890" }));
+    console.log("All Contacts After editing", addressBook.getAllContacts());
+    console.log(addressBook.deleteContact("Rakesh"));
+    console.log("All Contacts After Deleting:", addressBook.getAllContacts());
+    console.log("Total Contacts:", addressBook.countContacts()); 
+
+    const duplicateContact = new Contact(
+       "Sakshi", "Verma", "Bhopal", "India", "In", "100001", "9998887771","sakshi02v@gmail.com"
+    );
+    console.log(addressBook.addContact(duplicateContact)); 
+} catch (error) {
+    console.error("Error:", error.message);
+}
+
+try {
+    const invalidContact = new Contact(
+        "sa", "ver", "abc", "In", "I", "100A01", "12345", "invalid-email"
+    );
+    console.log(addressBook.addContact(invalidContact));
+} catch (error) {
+    console.error("Error:", error.message);
+}
--- src/services/AddressBook.js
+++ src/services/AddressBook.js
@@ -6,12 +6,52 @@ class AddressBook {
     }
 
     addContact(contact) {
+        const isDuplicate = this.contacts.filter(c => 
+            c.firstName.toLowerCase() === contact.firstName.toLowerCase() &&
+            c.lastName.toLowerCase() === contact.lastName.toLowerCase()
+        ).length > 0;
+
+        if (isDuplicate) {
+            throw new Error("Duplicate Contact! A person with the same name already exists.");
+        }
+
         this.contacts.push(contact);
         return "Contact added successfully!";
     }
 
     getAllContacts() {
         return this.contacts;
+    } 
+
+    findContactByName(name) {
+        return this.contacts.find(c => c.firstName.toLowerCase() === name.toLowerCase());
+    }
+
+    editContact(name, updatedDetails) {
+        let contact = this.findContactByName(name);
+        if (!contact) {
+            throw new Error("Contact not found!");
+        }
+
+        Object.keys(updatedDetails).forEach(key => {
+            if (updatedDetails[key]) {
+                contact[key] = updatedDetails[key];
+            }
+        });
+
+        return "Contact updated successfully!";
+    } 
+    deleteContact(name) {
+        const index = this.contacts.findIndex(c => c.firstName.toLowerCase() === name.toLowerCase());
+        if (index === -1) {
+            throw new Error("Contact not found!");
+        }
+        this.contacts.splice(index, 1);
+        return "Contact deleted successfully!";
+    } 
+
+    countContacts() {
+        return this.contacts.reduce((count) => count + 1, 0);
     }
 }
 
