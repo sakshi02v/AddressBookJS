207f179d023f862ecd131cfb3e1a0f569d4cefa1
Sakshi Verma <sakshi02v@gmail.com>
2025-03-05 11:04:06 +0530

    [Sakshi Verma] Add. Ability to ensure Valid Contacts are added

--- src/index.js
+++ src/index.js
@@ -3,8 +3,21 @@ const Contact = require('./models/Contact');
 
 const addressBook = new AddressBook();
 
-const contact1 = new Contact(
-    "Anand", "Soni", "Bhopal", "India", "In", "10001", "1234567890", "anandsoni9165@gmail.com"
-);
-console.log(addressBook.addContact(contact1));
-console.log(addressBook.getAllContacts());
+try {
+    const contact1 = new Contact(
+        "Anand", "Soni", "Bhopal", "India", "In", "100001", "9876543210", "anandsoni9165@gmail.com"
+    );
+    console.log(addressBook.addContact(contact1));
+    console.log(addressBook.getAllContacts());
+} catch (error) {
+    console.error("Error:", error.message);
+}
+
+try {
+    const invalidContact = new Contact(
+        "an", "son", "abc", "In", "I", "100A01", "12345", "invalid-email"
+    );
+    console.log(addressBook.addContact(invalidContact));
+} catch (error) {
+    console.error("Error:", error.message);
+}
