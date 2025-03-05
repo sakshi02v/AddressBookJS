da87173fa4ee97c99457311fa8b23827d1b0b759
Sakshi Verma <sakshi02v@gmail.com>
2025-03-05 11:15:55 +0530

    [Sakshi Verma]Add. Ability to find existing contact person using their name and edit it

--- src/index.js
+++ src/index.js
@@ -13,7 +13,9 @@ try {
         "Rakesh", "Soni", "Bhopal", "India","In","1000000","9197979988","rakeshsoni395@gmail.com"
     )
     console.log(addressBook.addContact(contact2)); 
-    console.log("All Contacts", addressBook.getAllContacts());
+    console.log("All Contacts Before editing", addressBook.getAllContacts());
+    console.log(addressBook.editContact("Rakesh", { address: "Pipariya", city: "New York", phone: "9234567890" }));
+    console.log("All Contacts After editing", addressBook.getAllContacts());
 } catch (error) {
     console.error("Error:", error.message);
 }
