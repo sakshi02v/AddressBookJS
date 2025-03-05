82d1455b2a1452747b8dd20854471389a0526427
Sakshi Verma <sakshi02v@gmail.com>
2025-03-05 11:08:55 +0530

    [Sakshi Verma]Add. Ability to create a New Address Book array and add new Contacts to it

--- src/index.js
+++ src/index.js
@@ -7,8 +7,13 @@ try {
     const contact1 = new Contact(
         "Anand", "Soni", "Bhopal", "India", "In", "100001", "9876543210", "anandsoni9165@gmail.com"
     );
-    console.log(addressBook.addContact(contact1));
-    console.log(addressBook.getAllContacts());
+    console.log(addressBook.addContact(contact1)); 
+
+    const contact2 = new Contact(
+        "Rakesh", "Soni", "Bhopal", "India","In","1000000","9197979988","rakeshsoni395@gmail.com"
+    )
+    console.log(addressBook.addContact(contact2)); 
+    console.log("All Contacts", addressBook.getAllContacts());
 } catch (error) {
     console.error("Error:", error.message);
 }
